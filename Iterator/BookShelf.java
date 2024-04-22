79242d1a0412ab779e9158cbcefb21c5598b63e8
俊杰 温 <3065225677@qq.com>
2024-04-22 14:59:43 +0800

    改用Vector维护书架

--- Iterator/BookShelf.java
+++ Iterator/BookShelf.java
@@ -1,18 +1,21 @@
+import java.awt.print.Book;
+import java.util.Vector;
+
 public class BookShelf implements Aggregate
 {
-    private Book[] books;
+    private Vector<Book> books;
     private int last=0;
     public BookShelf(int maxSize)
     {
-        this.books=new Book[maxSize];
+        this.books=new Vector<>(maxSize);
     }
     public Book getBookAt(int index)
     {
-        return books[index];
+        return books.get(index);
     }
     public void appendBook(Book book)
     {
-        this.books[last]=book;
+        books.add(book)
         last++;
     }
     public int getLength()
